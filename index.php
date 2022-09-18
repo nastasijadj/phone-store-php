3f4ec5a505ea97eef798ab19a5f34770818abe32
nastasijadj <nastasijadjordjevic98@gmail.com>
2022-09-18 22:56:29 +0200

    Svi telefoni na pocetnoj strani

--- index.php
+++ index.php
@@ -19,6 +19,36 @@
         <a href="FORMS/add.php"><button class="btn btn-primary" id="novi-btn">NOVI TELEFON</button></a>
 
 
+        <!-- Prikaz telefona -->
+        <div class="telefoni-prikaz">
+
+            <?php
+            require 'DB/DBConnection.php';
+
+            $SQL = "select telefon.id, telefon.model, telefon.dijagonala, telefon.ram, telefon.interna, telefon.cena, proizvodjac.naziv 
+            from telefon join proizvodjac on telefon.proizvodjac_id = proizvodjac.id";
+
+            $DATA = $connection->query($SQL);
+
+            while ($telefon = $DATA->fetch_object()) {
+            ?>
+                <div class="d-telefon">
+                    <h2 class="text-center text-primary"><?php echo $telefon->naziv . " " . $telefon->model ?></h2>
+                    <h3 class="text-center text-danger"><?php echo $telefon->cena ?> EUR</h3>
+                    <img id="slika-tel" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsiAkfek7FvnCVFfh_JfN9oNTk1Cu7lMPeyA&usqp=CAU">
+                    <h5 class="text-center">RAM: <?php echo $telefon->ram ?></h5>
+                    <h5 class="text-center">Interna memorija: <?php echo $telefon->interna ?></h5>
+                    <h5 class="text-center">Dijagonala: <?php echo $telefon->dijagonala ?></h5>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+
+
+
+
+
     </div>
 </body>
 
